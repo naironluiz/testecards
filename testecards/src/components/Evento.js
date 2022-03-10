d7e335f2f2a92c7a2786b31b0a2855f5210a7080
nairon.luiz <62431107+naironluiz@users.noreply.github.com>
2022-03-10 09:56:17 -0300

    Update Evento.js
    Adicionando um segundo evento no mesmo botão

--- testecards/src/components/Evento.js
+++ testecards/src/components/Evento.js
@@ -5,12 +5,19 @@ import Button from "./evento/Button";
 const Evento = () => {
   //1.Declara a função a ser disparada em um evento
   const meuEvento = () => {
-    console.log("Opa! Fui ativado");
+    console.log("Ativando primeiro evento");
+  };
+
+  const segundoEvento = () => {
+    console.log("Ativando segundo evento");
   };
   return (
     <>
       <p>Clique para disparar um evento</p>
+      {/* Utilizando o botão com evento onclick em prop */}
       <Button event={meuEvento} text="primeiro evento" />
+      {/*  pode se usar outra função no mesmo prop de evento, o mesmo código de origem que é o button on click em prop*/}
+      <Button event={segundoEvento} text="segundo evento" />
       {/* Adiciona a função criada a cima onclick */}
       <button onClick={meuEvento}>Ativar</button>
     </>
